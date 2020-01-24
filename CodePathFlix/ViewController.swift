22cd95d823a9d8d6788e658f406a1ae9af9d9d20
Alan Cao <alanpcao@gmail.com>
2020-01-23 17:29:28 -0800

    Initial Commit

---
+++ CodePathFlix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePathFlix
+//
+//  Created by Alan Cao on 1/23/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
