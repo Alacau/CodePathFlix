71b19028160cf318478b4925eb12f3d6a5b24fe8
Alan Cao <alanpcao@gmail.com>
2020-01-30 19:18:56 -0800

    finished unit 2

---
+++ CodePathFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  CodePathFlix
+//
+//  Created by Alan Cao on 1/30/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
